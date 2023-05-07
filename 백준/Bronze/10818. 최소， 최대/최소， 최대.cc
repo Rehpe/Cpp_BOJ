5ed21452c3eaad8c873c9c60c39d0137cb350d75
Rehpe <85483858+Rehpe@users.noreply.github.com>
2023-05-08 03:05:49 +0900

    [Bronze III] Title: 최소, 최대, Time: 472 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -3,7 +3,6 @@ using namespace std;
 
 int N;
 int x;
-vector<int> nums;
 int maxNum;
 int minNum;
 
@@ -15,7 +14,6 @@ int main()
 	for (int i  = 0; i < N; i++)
 	{
 		cin >> x;
-		nums.push_back(x);
 		maxNum = max(x, maxNum);
 		minNum = min(x, minNum);
 	}
