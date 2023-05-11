188452601dd39b551134879ef40b4256142f3eac
Rehpe <85483858+Rehpe@users.noreply.github.com>
2023-05-12 03:46:31 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -4,19 +4,19 @@ using namespace std;
 int L;
 string s;
 long long ret;
+#define M 1234567891;
 
 int main()
 {
 	cin >> L >> s;
 
+	long long powNum = 1;
+
 	for (int i = 0; i < L; i++)
 	{
-		int alphabet = s[i] -'a' + 1;	// 'a' == 1;
-		long long powNum = pow(31, i);
-		ret += alphabet * powNum;
+		int alphabet = s[i] - 'a' + 1;	// 'a' == 1;
+		ret = (ret + alphabet * powNum) % M;
+		powNum = (powNum * 31) % M;
 	}
-
-	ret %= 1234567891;
-
 	cout << ret;
 }
