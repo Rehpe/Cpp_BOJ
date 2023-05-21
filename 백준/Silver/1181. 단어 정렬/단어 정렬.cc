d3f06b0b707d10222c71d6371d458bf39b0549ce
Rehpe <85483858+Rehpe@users.noreply.github.com>
2023-05-21 23:44:05 +0900

    [Silver V] Title: 단어 정렬, Time: 44 ms, Memory: 4648 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+string s;
+vector<string> vec;
+
+bool cmp(string a, string b)
+{
+	if (a.size() == b.size())
+		return a < b;
+	else
+		return a.size() < b.size();
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> s;
+		vec.push_back(s);
+	}
+
+	sort(vec.begin(), vec.end(), cmp);
+
+	vec.erase(unique(vec.begin(), vec.end()), vec.end());
+
+	for (string s : vec)
+	{
+		cout << s << '\n';
+	}
+}
