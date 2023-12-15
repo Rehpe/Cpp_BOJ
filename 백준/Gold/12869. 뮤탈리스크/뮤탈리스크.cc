ff0d6abd59ff884e416603c85ae55863f2da858b
Rehpe <85483858+Rehpe@users.noreply.github.com>
2023-12-15 16:48:16 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 4200 KB -BaekjoonHub

---
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+int dp[64][64][64], visited[64][64][64];		// 최대 체력이 60이므로
+int attack[6][3] =
+{
+	{9, 3, 1},
+	{9, 1, 3},
+	{1, 3, 9},
+	{1, 9, 3},
+	{3, 9, 1},
+	{3, 1, 9}
+};
+int scv[3];
+
+struct A
+{
+	int a, b, c;			// tuple은 사용법이 복잡하므로 구조체로 간단하게 활용
+};
+
+queue<A> q;
+
+
+int solve(int a, int b, int c)
+{
+	// 방문처리
+	visited[a][b][c] = 1;
+	q.push({ a, b, c });
+
+	while (q.size())
+	{
+		int a = q.front().a;
+		int b = q.front().b;
+		int c = q.front().c;
+		
+		q.pop();
+
+		if (visited[0][0][0]) break;
+		for (int i = 0; i < 6; i++)
+		{
+			// 음수로 떨어지지 않게 max 0 처리
+			int na = max(0, a - attack[i][0]);
+			int nb = max(0, b - attack[i][1]);
+			int nc = max(0, c - attack[i][2]);
+
+			if (visited[na][nb][nc]) continue;
+			visited[na][nb][nc] = visited[a][b][c] + 1;
+			q.push({ na, nb, nc });
+		}
+	}
+
+	return visited[0][0][0] - 1;	// -1을 해주는 이유는 현재 체력에서 1로 시작했기 때문
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> scv[i];
+	}
+
+	cout << solve(scv[0], scv[1], scv[2]);
+	
+	return 0; 
+}
