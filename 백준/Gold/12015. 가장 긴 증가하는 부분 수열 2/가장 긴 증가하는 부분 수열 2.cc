d865d751c64eaa4eb113cb824035216ed8421ad2
Rehpe <85483858+Rehpe@users.noreply.github.com>
2024-03-15 15:24:36 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 164 ms, Memory: 12200 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr_size;
+int A[1000001];
+vector<int> L;		// L[i] =  A 배열에서 i 길이의 수열을 가지는 가장 작은 원소
+int ans;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> arr_size;
+
+	for (int i = 1; i <= arr_size; i++)
+	{
+		cin >> A[i];
+
+		if (L.empty() || L.back() < A[i])
+		{
+			L.push_back(A[i]);
+		}
+		else
+		{
+			int idx = lower_bound(L.begin(), L.end(), A[i]) - L.begin();
+			L[idx] = A[i];
+		}
+	}
+	
+	cout << L.size();
+
+	return 0;
+}
