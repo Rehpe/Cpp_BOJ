adc872ba49799292de049a9b3f017cc654c095d0
Rehpe <85483858+Rehpe@users.noreply.github.com>
2024-04-04 16:46:48 +0900

    [Gold IV] Title: N-Queen, Time: 1624 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -5,46 +5,43 @@ using namespace std;
 #define time aaa
 
 int N;
-int row[15];
 int ans;
+int isUsedCol[15];	// 각 열에 대응함
+int isUsedRightUp[30]; // 우상향 대각선 칸에 대응함 (x+y 값이 같음)
+int isUsedRightDown[30]; // 우하향 대각선 칸에 대응함 (x-y 값이 같음)
 
 void queen(int cnt)
 {
-	// n번째 행까지 퀸을 다 놓았다면 ans++
 	if (cnt == N)
 	{
 		ans++;
 		return;
 	}
 
-
 	for (int i = 0; i < N; i++)
 	{
-		// 2차원 배열을 1차원 배열로 표현하기 위해 row의 idx를 행, 값을 열로 나타낸다.
+		// i열에 이미 퀸이 있거나 // 우상향 대각선에 걸리거나 // 우하향 대각선에 걸리거나 (인덱스 음수 방지)
+		if (isUsedCol[i] || isUsedRightUp[cnt + i] || isUsedRightDown[cnt - i + N - 1])
+		{	
+			// 넘기기
+			continue;
+		}
 
-		// cnt번째 행 퀸 위치를 정함
-		row[cnt] = i;
-		
-		bool able = true;
-		// cnt 이전 열까지 놓았던 퀸의 위치를 바탕으로 가로-세로-대각선을 확인해서 
-		// 정한 위치(row[cnt] = i)에 퀸을 놓는 것이 가능한지 체크함
-		for (int j = 0; j < cnt; j++)
-		{
-			// 열이 같거나         // 오른쪽 대각선에 걸리거나 // 왼쪽 대각선에 걸리거나
-			if (row[cnt] == row[j] || cnt - i == j - row[j] || cnt + i == j + row[j])
-			{
-				able = false;
-				break;
-			}
-		}
-		// 위의 모든 경우에서 가능한 경우에만 실제로 퀸을 놓고 다음 행으로 넘어감
-		if (able)
-		{
-			queen(cnt + 1);
-		}
+		// 위 조건에 걸리지 않는다면, 현재 놓을 퀸의 좌표를 바탕으로 isUsed 시리즈들을 채운다
+		isUsedCol[i] = 1;
+		isUsedRightUp[cnt + i] = 1;
+		isUsedRightDown[cnt - i + N - 1] = 1;
+
+		// 다음 행(cnt + 1)으로 재귀호출
+		queen(cnt + 1);
+
+		// 점유했던 칸을 취소한다
+		isUsedCol[i] = 0;
+		isUsedRightUp[cnt + i] = 0;
+		isUsedRightDown[cnt - i + N - 1] =0;
 	}
-
 }
+
 
 
 int main()
