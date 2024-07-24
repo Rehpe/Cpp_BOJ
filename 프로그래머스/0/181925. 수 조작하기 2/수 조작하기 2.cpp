321fe4e54db1c91df062aaf06f93ee332960b1e5
Rehpe <85483858+Rehpe@users.noreply.github.com>
2024-07-24 20:49:59 +0900

    [level 0] Title: 수 조작하기 2, Time: 1.33 ms, Memory: 6.79 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+map<int, char> m = {{1,'w'}, {-1,'s'}, {10,'d'}, {-10,'a'}};
+
+string solution(vector<int> numLog) 
+{
+    string answer = "";
+    
+    for(int i = 1; i < numLog.size(); i++)
+    {
+        int sub = numLog[i] - numLog[i-1];
+        
+        answer += m[sub];
+    }
+    
+    return answer;
+}
