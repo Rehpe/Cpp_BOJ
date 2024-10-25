509da44ef5b4877840da6e75f5a5c856cd06aad3
Rehpe <85483858+Rehpe@users.noreply.github.com>
2024-10-26 00:53:40 +0900

    [level 2] Title: 올바른 괄호, Time: 0.59 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,25 @@
+#include<string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+
+    stack<char> stk;
+    
+    for(auto c: s)
+    {
+        if(c == '(')
+            stk.push(c);
+        else
+        {
+            if(stk.empty()) return false;
+            stk.pop();
+        }
+    }
+
+    return stk.empty();
+}
